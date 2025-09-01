b8b542188187667b5a9e4a3cd46924fe2175c672
Ash <ashley@codecapers.com.au>
2025-09-02 07:57:18 +1000

    Add fps counter.

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import FPSStats from 'react-fps-stats';
 import { Gallery } from './gallery.jsx';
 
 function App() {
   return (
     <div style={{ padding: '20px' }}>
+      <FPSStats 
+          top="auto"
+          left="auto"
+          width={300}
+          height={100}
+          right={30}
+          bottom={10}
+      />
       <h1>Simple Image Gallery</h1>
       <Gallery />
     </div>
